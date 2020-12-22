1bb69df91ebc52d6c1049b567bc08807790c0dcf
manstie <manstie@hotmail.com>
2020-12-22 18:17:25 +0800

    exactly 600 characters with comment 😎

--- vsinder.c
+++ vsinder.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#define _ int
-#define k long
+#define L int
 #define O char
-#define X return
-#define Y while
-_ x(_*x){*x*=2;}O*s=" world!";_ u(_ i){_ b=1;O c=i--;for(;c>i;c=s[-(i---8)])x(&b);X b;}_ l(_ c){_ t=0,i=8;Y(--i)t+=(u(i+1)&c)>>i;t+=c&1;X t;}k p(k p,_ x){_ q=p;Y(x--){p*=q;}X p;}_ c(_ x){X 32-(13*p(x,6))/15120+(349*p(x,5))/8640-(6379*p(x,4))/8640+(11705*p(x,3))/1728-(283729*p(x,2))/8640+(334523*p(x,1))/4320-(125173*x)/2520;}_ v(O*o){O x='\xFF';X (O)(100*(l(o[0]^x))+10*(l(o[1]^x))+(l(o[2]^x)));}main(){O*s="\xFF ~\xFB\xFF\xDF\xFE\xFF\0\x7F\xDF\xFD\xFF""cq";O b[6]={v(s),v(3+s),v(s+6),0,v(9+s),v(s+12)};s=(O[9]){c(0),c(9),c(8),c(5),c(6)+1,c(4),b[1],c(7)-1,c(1)-1};printf("%s%s%s",b,b+2,b+4);}
+#define V return
+#define E while
+O*s=" world!";L b(L i){L b=1;O c=i--;for(;c>i;c=s[-(i---8)])b*=2;V b;}L h(L c){L u=0,i=8;E(--i)u+=(b(i+1)&c)>>i;u+=c&1;V u;}L p(L p,L x){L q=p;E(x--){p*=q;}V p;}L c(L x){V 87+p(x,4)/15-25*p(x,3)/24+71*p(x,2)/12-371*p(x,1)/24+1411*x/60;}L t(O*o){V (O)(100*h(*o)+10*h(o[1])+h(o[2]));}main(){O*s="\0\x7F!@\0  \0\xFF   \0:9";O b[6]={t(s),t(3+s),t(s+6),0,t(9+s),t(s+12)};s=(O[9]){s[6],c(6),c(5)+1,c(2)-1,c(3)+1,c(1)+1,b[1],c(4),s[2]};printf("%s%s%s",b,b+2,b+4);}
+//github.com/manstie/vsinder-obfuscated-c/blob/main/vsinder.c
--- vsinder_comments.c
+++ vsinder_comments.c
@@ -1,50 +1,40 @@
 #include <stdio.h>
-#define _ int
-#define __ long
+#define L int
 #define O char
-#define X return
-#define Y while
-//multiplies a int pointer by 2
-_ x(_*x){*x*=2;}
+#define V return
+#define E while
 //red herring
 O*s = " world!";
 //returns 2^i apart from <= 1 which returns 0
-_ binaryIndex(_ i)
+L binaryIndex(L i)
 {
-    _ b = 1; O c = i--;
+    L b = 1; O c = i--;
     for(;c > i; c=s[-(i---8)])
-        x(&b);
-    X b;
+        b*=2;
+    V b;
 }
-_ countHowManyBinaryOnesAreInACharacter(_ c)
+L countHowManyBinaryOnesAreInACharacter(L c)
 {
-    _ cumulator = 0, i = 8;
-    Y(--i) cumulator+= (binaryIndex(i+1) & c)>>i;
+    L cumulator = 0, i = 8;
+    E(--i) cumulator+= (binaryIndex(i+1) & c)>>i;
     cumulator+=c&1;
-    X cumulator;
+    V cumulator;
 }
 
 //returns p to the power of x+1
-__ p(__ p,_ x){_ q=p;Y(x--){p*=q;}X p;}
+L p(L p,L x){L q=p;E(x--){p*=q;}V p;}
 
-//https://www.wolframalpha.com/input/?i=curve+fitting+%7B0%2C+32%7D%2C+%7B1%2C+33%7D+%7B4%2C+100%7D%2C+%7B5%2C+105%7D%2C+%7B6%2C+110%7D%2C+%7B7%2C+114%7D%2C+%7B8%2C+115%7D%2C+%7B9%2C+118%7D
-_ curve(_ x)
+//https://www.wolframalpha.com/input/?i=curve+fitting+%7B1%2C+100%7D%2C+%7B2%2C+105%7D%2C+%7B3%2C+110%7D%2C+%7B4%2C+114%7D%2C+%7B5%2C+115%7D%2C+%7B6%2C+118%7D
+L curve(L x)
 {
-     X 32-(13*p(x,6))/15120
-     + (349*p(x,5))/8640
-     - (6379*p(x,4))/8640
-     + (11705*p(x,3))/1728
-     - (283729*p(x,2))/8640
-     + (334523*p(x,1))/4320
-     - (125173*x)/2520;
+     V 87 + p(x,4)/15 - 25*p(x,3)/24 + 71*p(x,2)/12 - 371*p(x,1)/24 + 1411*x/60;
 }
 
-_ threeCharsToNum(O*o)
+L threeCharsToNum(O*o)
 {
-    O x='\xFF';
-    X (O)(100*(countHowManyBinaryOnesAreInACharacter(o[0]^x))
-    + 10*(countHowManyBinaryOnesAreInACharacter(o[1]^x))
-    + (countHowManyBinaryOnesAreInACharacter(o[2]^x)));
+    V (O)(100*countHowManyBinaryOnesAreInACharacter(*o)
+    + 10*countHowManyBinaryOnesAreInACharacter(o[1])
+    + countHowManyBinaryOnesAreInACharacter(o[2]));
 }
 
 main()
@@ -53,7 +43,7 @@ main()
     //obfuscate by generating the numbers using countHowManyBinaryOnesAreInACharacter
     //then multiplying them by the base10 index 2 1 0
     //each character will be xored
-    O*s="\xFF ~\xFB\xFF\xDF\xFE\xFF\0\x7F\xDF\xFD\xFF""cq";
+    O*s="\0\x7F!@\0  \0\xFF   \0:9";
     O b[6] = {threeCharsToNum(s),
         threeCharsToNum(3 + s),
         threeCharsToNum(s + 6),
@@ -61,9 +51,9 @@ main()
         threeCharsToNum(9 + s),
         threeCharsToNum(s + 12)
     };
-    s=(O[9]){curve(0), curve(9), curve(8), curve(5), curve(6)+1, curve(4), b[1], curve(7)-1, curve(1)-1};
-    // so I was planning on adding "s" to the printf statement here but apparently IT DOESN'T NEED IT?
-    // somehow when i set s=... it appends to b + 4
-	// this is great, it's somehow obfuscated such that even I don't understand it
+    s=(O[9]){s[6], curve(6), curve(5)+1, curve(2)-1, curve(3)+1, curve(1)+1, b[1], curve(4), s[2]};
+    // so I was planning on adding "s" to the printf statement here but it doesn't need it
+    // because I initialize my character arrays manually without a null terminator
+	// meaning it will continue to read the stack, and the stack must contain nulls after for it to terminate
 	printf("%s%s%s", b, b+2, b+4);
 }
